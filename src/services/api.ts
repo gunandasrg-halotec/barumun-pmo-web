78fb77dfc0d05e6e970a5685c1e740de93e9d809
Ezra Dalimunthe <ezra.dalimunthe@gmail.com>
2026-06-15 01:34:19 +0700

    add  auto refresh token feature  so expired token can be refreshed before expired.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,28 +1,55 @@
-import axios, { AxiosError, type AxiosInstance } from 'axios';
+/// <reference types="vite/client" />
 
-const BASE_URL = '/api';
+import axios, { AxiosError, type AxiosInstance } from "axios";
+
+const BASE_URL = import.meta.env.VITE_BACKEND + "/api/v1";
 
 const api: AxiosInstance = axios.create({
   baseURL: BASE_URL,
-  headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+  headers: { "Content-Type": "application/json", Accept: "application/json" },
   withCredentials: true,
 });
 
 // Attach token from localStorage
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('auth_token');
+  const token = localStorage.getItem("auth_token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
 // Redirect to login on 401
 api.interceptors.response.use(
-  (res) => res,
+  (res) => {
+    const headers = res.headers as any;
+    let authField: string | undefined;
+
+    if (headers) {
+      if (typeof headers.get === "function") {
+        authField =
+          headers.get("Authorization") ?? headers.get("authorization");
+      } else {
+        authField = headers["Authorization"] ?? headers["authorization"];
+      }
+    }
+    //when token is expired but still in refresh date
+    //update the token.
+    
+    if (authField && authField.startsWith("Bearer ")) {
+      const newToken = authField.replace(/^Bearer\s+/, "");
+      localStorage.setItem("auth_token", newToken);
+    }
+    return res;
+  },
   (error: AxiosError) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem('auth_token');
-      localStorage.removeItem('auth_user');
-      window.location.href = '/login';
+      const current_token = localStorage.getItem("auth_token");
+      //user has been logged in but
+      //account is deactivate or
+      if (current_token) {
+        localStorage.removeItem("auth_token");
+        localStorage.removeItem("auth_user");
+        // window.location.href = "/login";
+      }
     }
     return Promise.reject(error);
   }
